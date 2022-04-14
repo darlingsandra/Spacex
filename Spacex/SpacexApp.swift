fe59bfc570c33452d730b9a80f05d3950cde8627
darlingsandra <darlingsandra@yandex.ru>
2022-04-14 20:18:12 +0700

    Initial Commit

---
+++ Spacex/SpacexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacexApp.swift
+//  Spacex
+//
+//  Created by Александра Широкова on 14.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpacexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
